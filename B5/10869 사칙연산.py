649038fd0767c5a7bb5a33b514f43389fbeac338
jhy <yuns_97@naver.com>
2023-05-23 15:14:56 +0900

    230523 BOJ
    10869 사칙연산
    3003 킹, 퀸, 룩, 나이트, 비숍, 폰

---
+++ B5/10869 사칙연산.py
@@ -0,0 +1,6 @@
+a, b = input().split()
+
+a = int(a)
+b = int(b)
+
+print(a+b, a-b, a*b, int(a/b), a%b, sep="\n")
---
+++ B5/3003 킹 퀸 룩 비숍 나이트 폰.py
@@ -0,0 +1,12 @@
+# 순서대로 킹, 퀸, 룩, 비숍, 나이트, 폰 입력 받기
+a, b, c, d, e, f = input().split()
+
+# 입력 받은 a~f는 문자형이므로 정수형으로 변환
+a = int(a)
+b = int(b)
+c = int(c)
+d = int(d)
+e = int(e)
+f = int(f)
+
+print(1-a, 1-b, 2-c, 2-d, 2-e, 8-f)
